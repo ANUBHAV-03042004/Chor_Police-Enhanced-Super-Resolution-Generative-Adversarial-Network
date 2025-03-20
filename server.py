d5e81b9996d1c60e140afde9d21c7f647b2cb9e3
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-03-21 01:28:56 +0530

    Fix Vercel deployment

--- server.py
+++ server.py
@@ -64,4 +64,4 @@ def handler(event, context):
     return app(event, context)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
